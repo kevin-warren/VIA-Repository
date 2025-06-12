14054de6afb9da74605faba5cbd5bcdd524f6104
Kevin-Warren <kwarren2003@yahoo.com>
2025-06-12 14:31:18 -0400

    6/12 2:31

--- src/app/application-form/page.tsx
+++ src/app/application-form/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ApplicationFormPage() {
   return (
     <div>
-      <h1>Application Form!</h1>
+      <h1>Application Form...</h1>
       <p>This is the application form page.</p>
     </div>
   );

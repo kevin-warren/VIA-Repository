370877f143309cd913a6a5c08431c5517b5da90e
Kevin-Warren <kwarren2003@yahoo.com>
2025-06-12 10:23:55 -0400

    6/12 second

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,11 +1,23 @@
+import { Metadata } from 'next';
 import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 
-export default function Page({ params }: { params: { id: string } }) {
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default function Page({ params }: PageProps) {
   const post = posts.find((post) => post.id === params.id);
+
+  // Optional: handle post not found
+  if (!post) return <p>Post not found.</p>;
+
   return (
     <>
       <h1>Post</h1>
       <Post {...post} />
-    </>)
+    </>
+  );
 }

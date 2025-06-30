cd8cc83a351fadad3007fa3d13c44a27fc3d3d9d
Kevin-Warren <kwarren2003@yahoo.com>
2025-06-30 11:25:58 -0400

    6/30 11:25

--- auth.ts
+++ auth.ts
@@ -1,6 +1,37 @@
-import NextAuth from 'next-auth'
+import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+//import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const {auth, handlers, signIn, signOut} = NextAuth({
-    providers: [Google],
+    providers: [
+        // CredentialsProvider({
+        //     name: 'Sign in',
+        //     credentials: {
+        //         email: { 
+        //             label: 'Email', 
+        //             type: 'email',
+        //             placeholder: 'Enter your email',
+        //         },
+        //         password: { label: 'Password', type: 'password' },
+        //     },
+        //     async authorize(credentials) {
+        //         if (!credentials || !credentials.email || !credentials.password) 
+        //             return null;
+
+        //         const dbUser = await prisma.user.findFirst({
+        //             where: {
+        //                 email: credentials.email
+        //             },
+        //         });
+        //         if (dbUser && dbUser.password === credentials.password) {
+        //             const { password, createdAt, id, ...dbUserWithoutPassword } = dbUser;
+        //             return dbUserWithoutPassword as User;
+        //         }
+        //     },
+        // }),
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID as string,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+        })
+    ],
 })

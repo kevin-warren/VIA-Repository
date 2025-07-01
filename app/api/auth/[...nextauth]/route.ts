4fbc9de424b382be1076d9bbe1deee552459b8a3
Kevin-Warren <kwarren2003@yahoo.com>
2025-07-01 10:07:11 -0400

    7/1 10:07

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,12 @@
 import { handlers } from "../../../../auth";
 
+
 export const {GET, POST} = handlers;
+
+// import NextAuth from "next-auth";
+// import { authOptions } from "../../../lib/auth.config";
+
+// const handler = NextAuth(authOptions);
+
+// export const GET = handler;
+// export const POST = handler;
--- auth.ts
+++ auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 //import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaClient} from '@prisma/client';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+const prisma = new PrismaClient();
 
 export const {auth, handlers, signIn, signOut} = NextAuth({
     providers: [
@@ -34,4 +38,12 @@ export const {auth, handlers, signIn, signOut} = NextAuth({
             clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
         })
     ],
+    adapter: PrismaAdapter(prisma),
 })
+
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "./app/lib/auth.config"; // we split config out
+
+// export async function auth() {
+//   return await getServerSession(authOptions);
+// }
